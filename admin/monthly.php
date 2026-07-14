d55db0eecfbf1b174b3de645e321b250313bd34e
hupe13 <hupe13@t-online.de>
2026-07-14 21:39:38 +0200

    del old

--- admin/monthly.php
+++ admin/monthly.php
@@ -63,16 +63,6 @@ if ( ! ( is_multisite() && ! is_main_site() && is_plugin_active_for_network( $wi
 			echo '</p>';
 }
 
-// echo '<h4>' . wp_kses_post(
-// __( 'Monthly statistics - first access', 'wimb-and-block' ),
-// ) . '</h4>';
-//
-// echo '<p>' . wp_kses_post(
-// __( 'The browsers whose first access occurred during this month are counted for the month.', 'wimb-and-block' ),
-// ) . '</p>';
-//
-// echo wp_kses_post( wimbblock_statistic_new_month() );
-
 echo '<h4>' . wp_kses_post(
 	__( 'Statistics about systems in total', 'wimb-and-block' ),
 ) . '</h4>';
@@ -232,99 +222,6 @@ function wimbblock_statistic_month() {
 			$wimbblock_entries               = $wimb_datatable->get_results(
 				$wimb_datatable->prepare(
 					'SELECT COUNT(*) as blockrobot FROM %i WHERE time LIKE %s AND robots > 0 AND block > 0',
-					$wimbblock_table_name,
-					$search_date . '%'
-				),
-				ARRAY_A
-			);
-			$entries[ $month ]['blockrobot'] = $wimbblock_entries[0]['blockrobot'];
-
-			$entries[ $month ]['robots blocked in %'] =
-			wimbblock_prozent( $entries[ $month ]['blockrobot'], $entries[ $month ]['robots'] );
-		}
-
-		$header = array_fill_keys( array_keys( $entries[0] ), '' );
-		foreach ( $header as $key => $value ) {
-			$header[ $key ] = $key;
-		}
-		$header['month']               = __( 'month', 'wimb-and-block' );
-		$header['count']               = __( 'count', 'wimb-and-block' );
-		$header['blocked']             = __( 'blocked', 'wimb-and-block' );
-		$header['blocked in %']        = __( 'blocked in %', 'wimb-and-block' );
-		$header['robots']              = __( 'robots', 'wimb-and-block' );
-		$header['robots blocked in %'] = __( 'robots blocked in %', 'wimb-and-block' );
-		$header['blockrobot']          = __( 'robots blocked', 'wimb-and-block' );
-
-		array_unshift( $entries, $header );
-		return wimbblock_html_table( $entries );
-	} else {
-		return '';
-	}
-}
-
-function wimbblock_statistic_new_month() {
-	global $wimb_datatable;
-	if ( is_null( $wimb_datatable ) ) {
-		wimbblock_open_wpdb();
-	}
-	$wimbblock_wpdb_options = wimbblock_get_options_db();
-	$wimbblock_table_name   = $wimbblock_wpdb_options['table_name'];
-
-	$wimbblock_entries = $wimb_datatable->get_results(
-		$wimb_datatable->prepare(
-			"SELECT wimbdate FROM %i WHERE wimbdate != '' ORDER BY wimbdate ASC limit 1",
-			$wimbblock_table_name
-		),
-		ARRAY_A
-	);
-
-	if ( count( $wimbblock_entries ) > 0 ) {
-		$date1 = date_create( wp_date( 'Y-m-01' ) );
-		$date2 = date_create( wp_date( 'Y-m-01', strtotime( '01.' . substr( $wimbblock_entries[0]['wimbdate'], 2, 2 ) . '.20' . substr( $wimbblock_entries[0]['wimbdate'], 0, 2 ) ) ) );
-		$diff  = date_diff( $date1, $date2 );
-
-		$thismonth = wp_date( 'Y-m' );
-		$months    = range( 0, $diff->format( '%m' ) );
-		$entries   = array();
-
-		foreach ( $months as $month ) {
-			$entries[ $month ]['month']   = wp_date( 'F', strtotime( $thismonth . ' - ' . $month . ' month' ) );
-			$search_date                  = wp_date( 'ym', strtotime( $thismonth . ' - ' . $month . ' month' ) );
-			$wimbblock_entries            = $wimb_datatable->get_results(
-				$wimb_datatable->prepare(
-					'SELECT COUNT(*) as count FROM %i WHERE wimbdate LIKE %s',
-					$wimbblock_table_name,
-					$search_date . '%'
-				),
-				ARRAY_A
-			);
-			$entries[ $month ]['count']   = $wimbblock_entries[0]['count'];
-			$wimbblock_entries            = $wimb_datatable->get_results(
-				$wimb_datatable->prepare(
-					'SELECT COUNT(*) as blocked FROM %i WHERE wimbdate LIKE %s AND block > 0',
-					$wimbblock_table_name,
-					$search_date . '%'
-				),
-				ARRAY_A
-			);
-			$entries[ $month ]['blocked'] = $wimbblock_entries[0]['blocked'];
-
-			$entries[ $month ]['blocked in %'] =
-			wimbblock_prozent( $entries[ $month ]['blocked'], $entries[ $month ]['count'] );
-
-			$wimbblock_entries           = $wimb_datatable->get_results(
-				$wimb_datatable->prepare(
-					'SELECT COUNT(*) as robots FROM %i WHERE wimbdate LIKE %s AND robots > 0',
-					$wimbblock_table_name,
-					$search_date . '%'
-				),
-				ARRAY_A
-			);
-			$entries[ $month ]['robots'] = $wimbblock_entries[0]['robots'];
-
-			$wimbblock_entries               = $wimb_datatable->get_results(
-				$wimb_datatable->prepare(
-					'SELECT COUNT(*) as blockrobot FROM %i WHERE wimbdate LIKE %s AND robots > 0 AND block > 0',
 					$wimbblock_table_name,
 					$search_date . '%'
 				),
